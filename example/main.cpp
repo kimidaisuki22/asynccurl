57a72cb50a0e86a0ed00f16498c38958b5c78cc6
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-08-19 15:26:33 +0800

    add more details example.

--- example/main.cpp
+++ example/main.cpp
@@ -1,14 +1,36 @@
 #include "asynccurl/request.h"
 #include <asynccurl/executor.h>
+#include <memory>
 #include <spdlog/spdlog.h>
+#include <spdlog/stopwatch.h>
+#include <vector>
 int main() {
-  asynccurl::Request basic{"https://www.bing.com"};
+  const std::string url = "https://www.bing.com";
+  asynccurl::Request basic{url};
 
   asynccurl::Executor exec;
   SPDLOG_INFO("test block version");
   basic.execute_with_block();
+
+  const int loop_total = 20;
+  spdlog::stopwatch block_stopwatch;
+  for (int i = 0; i < loop_total; i++) {
+    SPDLOG_INFO("loop {}", i);
+    basic.execute_with_block();
+  }
+  SPDLOG_INFO("simple version: {}", block_stopwatch);
+
   SPDLOG_INFO("test async version");
-  exec.add_handle(basic.build_handle());
+
+  spdlog::stopwatch async_stopwatch;
+  std::vector<std::shared_ptr<asynccurl::Request>> requests;
+  for (int i = 0; i < loop_total; i++) {
+    auto res = std::make_shared<asynccurl::Request>(url);
+    requests.push_back(res);
+    exec.add_handle(res->build_handle());
+  }
 
   exec.run();
+  SPDLOG_INFO("async version: {}", async_stopwatch);
+  SPDLOG_INFO("finished.");
 }
