f3f651ec714160f79f168549e8868500e996b387
kimidaisuki22 <40718043+kimidaisuki22@users.noreply.github.com>
2023-08-19 18:55:50 +0800

    remove shared_ptr in task.

--- include/asynccurl/task.h
+++ include/asynccurl/task.h
@@ -28,18 +28,16 @@ public:
     constexpr std::suspend_always initial_suspend() noexcept { return {}; }
     Resume_parent final_suspend() noexcept { return {parent_}; }
 
-    void return_value(T value) { *result_value_ = std::move(value); }
+    void return_value(T value) { result_value_ = std::move(value); }
 
     [[noreturn]] static void unhandled_exception() { throw; }
     std::coroutine_handle<> parent_{};
-    std::shared_ptr<T> result_value_ = std::make_shared<T>();
+    T result_value_{};
   };
 
   using Handle = std::coroutine_handle<promise_type>;
 
-  explicit Task(const Handle coroutine) : coroutine_{coroutine} {
-    result_ = coroutine_.promise().result_value_;
-  }
+  explicit Task(const Handle coroutine) : coroutine_{coroutine} {}
 
   Task() = default;
   ~Task() {
@@ -52,7 +50,6 @@ public:
 
   Task(Task &&other) noexcept : coroutine_{other.coroutine_} {
     other.coroutine_ = {};
-    result_ = other.result_;
   }
   Task &operator=(Task &&other) noexcept {
     if (this != &other) {
@@ -60,7 +57,6 @@ public:
         coroutine_.destroy();
       coroutine_ = other.coroutine_;
       other.coroutine_ = {};
-      result_ = other.result_;
     }
     return *this;
   }
@@ -68,11 +64,10 @@ public:
   void set_parent(std::coroutine_handle<> hd) {
     coroutine_.promise().parent_ = hd;
   }
-  T get_result() { return *result_; }
+  T get_result() { return coroutine_.promise().result_value_; }
 
 private:
   Handle coroutine_{};
-  std::shared_ptr<T> result_;
 };
 
 // template <> class Task<void> {
